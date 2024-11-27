add65ac6c729c4145c6c1201804e78374a94ea36
diesel <139544760+suacho0107@users.noreply.github.com>
2024-11-28 01:25:32 +0900

    Revert "판별 업데이트(2)"
    This reverts commit 058c10aa66f08c0a567d9d169e35b66faab86873.

--- Assets/01.Scripts/Dialogue/DialogueManager.cs
+++ Assets/01.Scripts/Dialogue/DialogueManager.cs
@@ -29,7 +29,6 @@ public class DialogueManager : MonoBehaviour
     NPC npc; //= currentNPC
     public PlayerMove playerMove; //플레이어 FSM과 연결, 추가 코드
     StatueScore statueScore;
-    MuseumLobbyCSV csv;
 
     bool isDialogue = false;
     bool isNext = false; //특정 키 입력 대기
@@ -209,7 +208,6 @@ public class DialogueManager : MonoBehaviour
             if (!npc.isChecked && currentIndex == 0) // 첫 번째 상호작용(조사): 선지 2개 출력
             {
                 npc.isChecked = true;
-                npc.SaveNPCData();
                 Debug.Log("statue.isChecked == True");
             }
             else if (!npc.isChecked && currentIndex == 1)
@@ -231,13 +229,11 @@ public class DialogueManager : MonoBehaviour
                 {// 건드린다 --> 정답
                     npc.isJudged = true;
                     npc.isCorrect = true;
-                    //npc.SaveNPCData();
                 }
                 else
                 {// 건드린다 --> 오답
                     npc.isJudged = true;
                     npc.isCorrect = false;
-                    //npc.SaveNPCData();
                 }
             }
             else if (npc.isChecked && currentIndex == 2)
@@ -249,13 +245,11 @@ public class DialogueManager : MonoBehaviour
                 {// 이상 없음 --> 오답
                     npc.isJudged = true;
                     npc.isCorrect = false;
-                    //npc.SaveNPCData();
                 }
                 else
                 {// 이상 없음 --> 정답
                     npc.isJudged = true;
                     npc.isCorrect = true;
-                    //npc.SaveNPCData();
                 }
             }
             else if (npc.isChecked && currentIndex == 3)
@@ -295,39 +289,8 @@ public class DialogueManager : MonoBehaviour
         dialogues = null;
         isNext = false;
         isExplain = false;
-        npc.isInteract = true; // 미술관장
-        if(npc.dialogueFileName == "Tutorial2_dialogue")
-        {
-            npc.isTutoFin = true;
-            Debug.Log("isTutoFin True 테스트 완료 시 삭제할 로그");
-        }
-        if(npc.isStatue && npc.isChecked && npc.isJudged)
-        {
-            if(npc.isEnemy)
-            {
-                npc.result = true;
-            }
-            else
-            {
-                if (npc.isCorrect)
-                {
-                    if (npc.currentIndex == 2)
-                    {
-                        npc.result = true;
-                        Debug.Log("result True 테스트 완료 시 삭제할 로그");
-                    }
-                }
-                else
-                {
-                    if (npc.currentIndex == 3)
-                    {
-                        npc.result = true;
-                        Debug.Log("result True 테스트 완료 시 삭제할 로그");
-                    }
-                }
-            }
-        }
-        npc.SaveNPCData();
+        npc.isInteract = true;
+        npc.SaveTuto();
 
         dialoguePanel.SetActive(false);
         namePanel.SetActive(false);
