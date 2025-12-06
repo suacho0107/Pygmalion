8fcf71be18132eb39daf607aeb03b43482f1e50b
diesel <139544760+suacho0107@users.noreply.github.com>
2025-12-07 01:57:36 +0900

    fix 2일차 회사 로비 씬에 StageIndex 도서관으로 설정
     - UIManager.u_instance.StageIndex = 1;

---
+++ Assets/01.Scripts/Game/ChangeStateIndex.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeStateIndex : MonoBehaviour
+{
+    void Start()
+    {
+        UIManager.u_instance.stageIndex = 1;
+    }
+
+}
