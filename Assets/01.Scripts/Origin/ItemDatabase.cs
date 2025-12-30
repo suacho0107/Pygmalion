c07fdfb7a5d16628cf452502e6790818c3d7e3ea
swuMinju <minjupark22@swu.ac.kr>
2025-12-30 23:16:03 +0900

    feat 도서관 키오스크 및 사물함 상호작용 적용
    상호작용 후 아이템 습득까지 완료

--- Assets/01.Scripts/Origin/ItemDatabase.cs
+++ Assets/01.Scripts/Origin/ItemDatabase.cs
@@ -8,13 +8,15 @@ public class ItemDatabase : MonoBehaviour
     
     void Start()
     {
-        itemList.Add(new Item(10001, "Items_10", "A설명", "Itmes_10", Item.ItemType.Use));
-        itemList.Add(new Item(10002, "B이름", "B설명", "B이름", Item.ItemType.Use));
-        itemList.Add(new Item(20001, "C이름", "C설명", "C이름", Item.ItemType.Equip));
-        itemList.Add(new Item(10401, "열쇠꾸러미", "경비원에게 보여주자.", "열쇠꾸러미", Item.ItemType.Use));
-        itemList.Add(new Item(10402, "비타5000", "비타5000 설명", "비타5000", Item.ItemType.Use));
+        //itemList.Add(new Item(10001, "Items_10", "A설명", "Itmes_10", Item.ItemType.Use));
+        //itemList.Add(new Item(10002, "B이름", "B설명", "B이름", Item.ItemType.Use));
+        //itemList.Add(new Item(20001, "C이름", "C설명", "C이름", Item.ItemType.Equip));
+        itemList.Add(new Item(10401, "열쇠꾸러미", "정원의 경비원에게 보여주자.", "열쇠꾸러미", Item.ItemType.Use));
+        itemList.Add(new Item(10402, "비타5000", "요즘 인기 최고인 에너지드링크. 피로와 상처를 순식간에 회복시켜준다.", "비타5000", Item.ItemType.Use));
+        itemList.Add(new Item(10403, "포도주", "술과 축제의 신 디오니소스가 특별히 만든 포도주. 생명력이 깃들어 죽어가던 사람도 살아난다고 한다.", "포도주", Item.ItemType.Use));
         itemList.Add(new Item(10301, "정체불명의 조각들", "이게 뭐지?", "정체불명의 조각들", Item.ItemType.Quest));
-        itemList.Add(new Item(20101, "열람실 열쇠", "열람실 열쇠 설명", "열람실 열쇠", Item.ItemType.Use));
-        itemList.Add(new Item(20102, "회의실 열쇠", "회의실 열쇠 설명", "회의실 열쇠", Item.ItemType.Use));
+        itemList.Add(new Item(20101, "열람실 열쇠", "도서관 B1 열람실의 잠금을 해제할 수 있다.", "열람실 열쇠", Item.ItemType.Use));
+        itemList.Add(new Item(20102, "회의실A 열쇠", "도서관 1F 회의실A의 잠금을 해제할 수 있다.", "회의실A 열쇠", Item.ItemType.Use));
+        itemList.Add(new Item(20103, "수상한 액체가 든 병", "정체를 모르는 수상한 액체가 들어 있다. 실수로 조각상에 뿌리면 부식될 수 있으니 주의하자.", "수상한 액체가 든 병", Item.ItemType.Use));
     }    
 }
