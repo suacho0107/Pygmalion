c23a8fa5222ac10117737671402745e9af6680e2
diesel <139544760+suacho0107@users.noreply.github.com>
2026-06-12 15:33:23 +0900

    fix : Portrait2 프리팹 수정
     - 표정 변화 없는 등장인물 Portrait1과 같은 방식으로 Portrait2 수정
     - 이름/이미지(기본)
     - 미술관, 도서관 테스트용 최초 대화 파일에 portrait 1로 추가

--- Assets/01.Scripts/Dialogue/DialogueParser.cs
+++ Assets/01.Scripts/Dialogue/DialogueParser.cs
@@ -25,24 +25,6 @@ public class DialogueParser : MonoBehaviour
             List<string> eventList = new List<string>();
             List<string> skipList = new List<string>();
 
-            //do
-            //{
-            //    contextList.Add(row[2]);
-            //    eventList.Add(row[3]);
-            //    skipList.Add(row[4]);
-
-
-            //    if (++i < data.Length - 1)
-            //    {
-            //        row = data[i].Split(new char[] { ',' });
-            //    }
-            //    else
-            //    {
-            //        break;
-            //    }
-
-            //} while (row[0].ToString() == ""); //ID가 공백이면 sentence만 추가
-
             do
             {
                 portraitList.Add(row.Length > 2 ? int.Parse(row[2]) : 0); // 열의 길이를 체크하고 부족하면 빈 문자열 추가
